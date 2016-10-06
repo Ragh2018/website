ba637b3ac35a3c36995cc8b3c6283f9241ac3504
Ragh2018 <ragh1995@hotmail.com>
2016-10-07 00:23:13 +0530

    Django Project

---
+++ music/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Album(models.Model):
+    artist = models.CharField(max_length=100)
+    album_title=models.CharField(max_length=200)
+    genre = models.CharField(max_length=50)
+    album_logo=models.CharField(max_length=50)
+    def __str__(self):
+        return self.album_title +' - '+self.artist
+class song(models.Model):
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
+    file_type =models.CharField(max_length=10)
+    song_title =models.CharField(max_length=200)
