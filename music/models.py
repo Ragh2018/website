95cf09ca93058b465c017add6638af2360775ade
Ragh2018 <ragh1995@hotmail.com>
2016-10-07 14:40:09 +0530

    Adding song's to  the album

--- music/models.py
+++ music/models.py
@@ -7,9 +7,14 @@ class Album(models.Model):
     album_title=models.CharField(max_length=200)
     genre = models.CharField(max_length=50)
     album_logo=models.CharField(max_length=50)
+
     def __str__(self):
         return self.album_title +' - '+self.artist
+
 class song(models.Model):
     album = models.ForeignKey(Album,on_delete=models.CASCADE)
     file_type =models.CharField(max_length=10)
     song_title =models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.song_title
