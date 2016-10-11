32b7d13e8b42d58ebe89b22411b70ff54cf9f602
Ragh2018 <ragh1995@hotmail.com>
2016-10-11 23:19:22 +0530

    favorite view function

--- music/models.py
+++ music/models.py
@@ -16,5 +16,6 @@ class song(models.Model):
     file_type =models.CharField(max_length=10)
     song_title =models.CharField(max_length=200)
     is_favorite = models.BooleanField(default=False)
+
     def __str__(self):
         return self.song_title
