2d48a4e938dd1298bc1edf71b5a5d43d486f23ee
Ragh2018 <ragh1995@hotmail.com>
2016-11-11 15:06:06 +0530

    changes in index and views.py

--- music/urls.py
+++ music/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(),name='index'),
     #/music/712/
-    url(r'^(?P<album_id>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
 
 ]
